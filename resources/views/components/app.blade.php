ccf8ecb2e407f24ee242c901d0679f8f3996fb5d
William Huang <william.huang001@binus.ac.id>
2022-10-09 00:14:36 +0700

    test

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -6,32 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Link & Co') }}</title>
+    <title>{{ config('app.name', 'Barbatos Shop') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 </head>
 
-<body class="flex flex-col bg-green-900 min-h-screen">
+<body class="bg-light">
 
     <div class="">
         <div class="">
-            {{-- @include('layouts.navigation') --}}
+            @include('components.navigation')
         </div>
         <!-- Page Content -->
-        <main>
+        <main class="">
             {{ $slot }}
         </main>
     </div>
-
-    <script src="https://unpkg.com/flowbite@1.4.5/dist/flowbite.js"></script>
-    <script src="https://unpkg.com/flowbite@1.4.5/dist/datepicker.js"></script>
 </body>
 
 
---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg container">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavLightDropdown"
+            aria-controls="navbarNavLightDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavLightDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-light">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="d-flex gap-3">
+        <a href="{{ route('login.show') }}" class="text-decoration-none text-black">
+            Login
+        </a>
+        <a href="{{ route('register.show') }}" class="text-decoration-none text-black">
+            Register
+        </a>
+    </div>
+</nav>
