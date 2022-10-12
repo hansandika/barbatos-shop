ab958279bfa4b9522d8d532f08fabf421b9f1da4
Hans Andika <hansandika70@gmail.com>
2022-10-12 07:49:40 +0700

    add controller and migration for countries

---
+++ app/Models/Coutry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coutry extends Model
+{
+    use HasFactory;
+}
