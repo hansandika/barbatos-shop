0ae1b4e16d7c01e136143b7a0b2c342ed5b250c7
Hans Andika <hansandika70@gmail.com>
2022-10-26 15:09:32 +0700

    fix register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,23 +9,41 @@
                     @csrf
                     <div class="mb-3">
                         <label for="name" class="form-label fw-normal">Name</label>
-                        <input type="text" id="name" class="form-control" placeholder="Enter Your Name"
-                            name="name" required>
+                        <input type="text" id="name" class="form-control @error('name')is-invalid @enderror"
+                            placeholder="Enter Your Name" name="name" value="{{ old('name') }}" required>
+                        @error('name')
+                            <p class="mt-2 text-danger"><span class="fw-medium">Oops! </span>{{ $message }}
+                            </p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label fw-normal">Email address</label>
-                        <input type="email" id="email" class="form-control" placeholder="Enter Your Email"
-                            name="email" required>
+                        <input type="email" id="email" class="form-control @error('email')is-invalid @enderror"
+                            placeholder="Enter Your Email" name="email" value="{{ old('email') }}" required>
+                        @error('email')
+                            <p class="mt-2 text-danger"><span class="fw-medium">Oops! </span>{{ $message }}
+                            </p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label fw-normal">Password</label>
-                        <input type="password" id="password" class="form-control" name="password"
-                            placeholder="Enter Your Password" required>
+                        <input type="password" id="password"
+                            class="form-control @error('password')is-invalid @enderror" name="password"
+                            placeholder="Enter Your Password" value="{{ old('password') }}" required>
+                        @error('password')
+                            <p class="mt-2 text-danger"><span class="fw-medium">Oops! </span>{{ $message }}
+                            </p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="confirm-password" class="form-label fw-normal">Confirm Password</label>
-                        <input type="password" id="confirm-password" class="form-control" name="confirm-password"
-                            placeholder="Re-type Your Password" required>
+                        <input type="password" id="confirm-password"
+                            class="form-control @error('confirm-password')is-invalid @enderror" name="confirm-password"
+                            placeholder="Re-type Your Password" value="{{ old('confirm-password') }}"required>
+                        @error('confirm-password')
+                            <p class="mt-2 text-danger"><span class="fw-medium">Oops! </span>{{ $message }}
+                            </p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="gender" class="form-label fw-normal">Gender</label>
@@ -44,18 +62,28 @@
                         </div>
                     </div>
                     <div class="mb-3">
-                        <label for="dateOfBirth" class="form-label fw-normal">Date of Birth</label>
-                        <input type="date" class="form-control" name="dateOfBirth" placeholder="mm/dd/yy" required
-                            id="dateOfBirth">
+                        <label for="dob" class="form-label fw-normal">Date of Birth</label>
+                        <input type="date" class="form-control @error('dob')is-invalid @enderror" name="dob"
+                            placeholder="mm/dd/yy" value="{{ old('dob') }}" required id="dob">
+                        @error('dob')
+                            <p class="mt-2 text-danger"><span class="fw-medium">Oops! </span>{{ $message }}
+                            </p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="country" class="form-label fw-normal">Country</label>
-                        <select class="form-select" id="country" name="country">
+                        <select class="form-select @error('country')is-invalid @enderror" id="country" name="country">
                             <option disabled>select country</option>
                             @foreach ($countries as $country)
-                                <option value="{{ $country->country_code }}">{{ $country->country_name }}</option>
+                                <option value="{{ $country->id }}"
+                                    {{ $country->id == old('country') ? 'selected' : '' }}>
+                                    {{ $country->country_name }}</option>
                             @endforeach
                         </select>
+                        @error('country')
+                            <p class="mt-2 text-danger"><span class="fw-medium">Oops! </span>{{ $message }}
+                            </p>
+                        @enderror
                     </div>
                     <div class="d-flex justify-content-center">
                         <button type="submit" class="btn btn-outline-secondary" style="width:160px;">Register</button>
